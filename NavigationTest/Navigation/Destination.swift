e27b6fdbfe5f257980301190b27c35af4fd1ce93
Daniel Tull <dt@danieltull.co.uk>
2025-08-29 09:17:54 +0100

    Move destinations code

--- NavigationTest/Navigation/Destination.swift
+++ NavigationTest/Navigation/Destination.swift
@@ -14,3 +14,46 @@ public struct Destination {
     }
   }
 }
+
+// MARK: - Destinations
+
+public struct Destinations {
+  let values: [Destination]
+
+  fileprivate init(values: [Destination]) {
+    self.values = values
+  }
+
+  public init(@Destinations.Builder build: () -> Destinations) {
+    self = build()
+  }
+}
+
+extension Destinations {
+  @resultBuilder
+  public enum Builder {
+
+    public static func buildBlock() -> Destinations {
+      Destinations(values: [])
+    }
+
+    public static func buildExpression(_ expression: Destination) -> Destinations {
+      Destinations(values: [expression])
+    }
+
+    public static func buildExpression(_ expression: Destinations) -> Destinations {
+      expression
+    }
+
+    public static func buildPartialBlock(first: Destinations) -> Destinations {
+      first
+    }
+
+    public static func buildPartialBlock(
+      accumulated: Destinations,
+      next: Destinations
+    ) -> Destinations {
+      Destinations(values: accumulated.values + next.values)
+    }
+  }
+}
--- NavigationTest/Navigation/Navigator.swift
+++ NavigationTest/Navigation/Navigator.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 struct Navigator {
-  
+
   private let destinations: Destinations
 
   init(@Destinations.Builder destinations: () -> Destinations) {
@@ -18,45 +18,4 @@ struct Navigator {
 
 extension EnvironmentValues {
   @Entry var navigator: Navigator = Navigator {}
-}
-
-public struct Destinations {
-  let values: [Destination]
-
-  fileprivate init(values: [Destination]) {
-    self.values = values
-  }
-
-  public init(@Destinations.Builder build: () -> Destinations) {
-    self = build()
-  }
-}
-
-extension Destinations {
-  @resultBuilder
-  public enum Builder {
-
-    public static func buildBlock() -> Destinations {
-      Destinations(values: [])
-    }
-
-    public static func buildExpression(_ expression: Destination) -> Destinations {
-      Destinations(values: [expression])
-    }
-
-    public static func buildExpression(_ expression: Destinations) -> Destinations {
-      expression
-    }
-
-    public static func buildPartialBlock(first: Destinations) -> Destinations {
-      first
-    }
-
-    public static func buildPartialBlock(
-      accumulated: Destinations,
-      next: Destinations
-    ) -> Destinations {
-      Destinations(values: accumulated.values + next.values)
-    }
-  }
 }
