8e7dd4e04dcd23f836661191bfc7161eda03d73b
Daniel Tull <dt@danieltull.co.uk>
2025-08-29 09:34:50 +0100

    Extract public API to an extension

--- NavigationTest/Navigation/Route.swift
+++ NavigationTest/Navigation/Route.swift
@@ -1,17 +1,25 @@
 import RegexBuilder
 
 public struct Route<Value> {
-
   private let value: (Link) throws -> Value?
+}
 
-  public init<Output>(regex: Regex<Output>, transform: @escaping (Output) -> Value?) {
+extension Route {
+
+  public init<Output>(
+    regex: Regex<Output>,
+    transform: @escaping (Output) -> Value?
+  ) {
     self.value = { link in
       guard let match = try regex.wholeMatch(in: link.string) else { return nil }
       return transform(match.output)
     }
   }
 
-  public init<Output>(@RegexComponentBuilder regex: () -> Regex<Output>, transform: @escaping (Output) -> Value?) {
+  public init<Output>(
+    @RegexComponentBuilder regex: () -> Regex<Output>,
+    transform: @escaping (Output) -> Value?
+  ) {
     self.init(regex: regex(), transform: transform)
   }
 
